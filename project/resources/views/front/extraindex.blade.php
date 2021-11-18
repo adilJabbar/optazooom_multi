58952a7a147aaf1346caa01b5077d1f454af1283
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2021-11-18 22:21:24 +0500

    commit

--- project/resources/views/front/extraindex.blade.php
+++ project/resources/views/front/extraindex.blade.php
@@ -59,35 +59,7 @@
 		<!-- Phone and Accessories Area start-->
 	@endif
 
-	@if($ps->flash_deal == 1)
-		<!-- Electronics Area Start -->
-		<section class="categori-item electronics-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 remove-padding">
-						<div class="section-top">
-							<h2 class="section-title">
-								{{ $langg->lang244 }}
-							</h2>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="flash-deals">
-							<div class="flas-deal-slider">
-
-								@foreach($discount_products as $prod)
-									@include('includes.product.flash-product')
-								@endforeach
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- Electronics Area start-->
-	@endif
+	
 
 	@if($ps->large_banner == 1)
 		<!-- Banner Area One Start -->
@@ -502,6 +474,36 @@
 						
             </div>
 			<!-- End Featured Vendors -->
+
+			@if($ps->flash_deal == 1)
+		<!-- Electronics Area Start -->
+		<section class="categori-item electronics-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 remove-padding">
+						<div class="section-top">
+							<h2 class="section-title">
+								{{ $langg->lang244 }}
+							</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="flash-deals">
+							<div class="flas-deal-slider">
+
+								@foreach($discount_products as $prod)
+									@include('includes.product.flash-product')
+								@endforeach
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Electronics Area start-->
+	@endif
 
 			<!-- Testimonials -->
 			<div class="container">
