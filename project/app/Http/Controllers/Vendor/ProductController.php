3d489b4a47ece87f3b9c4c353f05f1c3a27ef203
adilJabbar <46170020+adilJabbar@users.noreply.github.com>
2021-11-23 11:28:51 +0500

    commot

--- project/app/Http/Controllers/Vendor/ProductController.php
+++ project/app/Http/Controllers/Vendor/ProductController.php
@@ -400,6 +400,9 @@ class ProductController extends Controller
             // }
 
 
+
+
+
                 $input['previous_price'] = ($input['previous_price'] / $sign->value);
                 $input['user_id'] = $user->id;
                 // Save Data
