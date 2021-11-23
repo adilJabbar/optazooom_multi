88cc28b08315c7d00cf25a1b7b7303607a5c7d90
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2021-11-23 18:42:05 +0500

    commit css

--- project/resources/views/front/vendorlist.blade.php
+++ project/resources/views/front/vendorlist.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.front')
 @section('content')
-<!-- search bar -->
-
-                   
-<!-- end serach bar -->
         <div class="vendorheader text-center">
             <div class="container ">
                 <h3>VENDORS</h3>
@@ -12,40 +8,37 @@
                </div>
             </div>
         </div>
-        <div class="justify-content-between">      
-                       <div class="toadjuctserach"  >
-                             <form action="#" method="GET">
-                                    <div style="border-bottom: 1px solid #0000006e; width: 205px;">
-                                     <input id="fsearch" style="border: none; box-shadow: none; padding-bottom: 8px; border-radius: 0;" type="text"  name="fsearch" value="" class="li-search-field" placeholder="Search a specific Vendor">
-                                    <input id="uri_seg" type="hidden" name="uri_seg" value="">
-                                        <input type="hidden" name="category" value="" class="li-search-field" placeholder="Search a specific Vendor">
-                                    <button id="btn" type="submit" class="li-search-btn"  style="border: none; box-shadow: none;padding-bottom: 8px; background:none;   border-radius: 0;"><i style="color:#f7b543;font-size: 24px;" class="fa fa-search"></i></button>
-                                    </div>
-                                </form>
-                        </div>
-                     </div>
-       <div class="container pt-60">
-           <div class="row userss">
-           @foreach ($users as $user)
-                <div class="col-lg-3">
-                    <div class="featured">
-						<div class="featured-vendor">
-							<a href="{{ route('front.vendor',str_replace(' ', '-', $user->shop_name)) }}">
-								<div class="cat-img">
-                                  <img src="{{ $user->photo ? asset('assets/images/users/'.$user->photo):asset('assets/images/noimage.png') }}" />
-                                  
-                               </div>
-
-							   <hr>
-								<h4 class="vendor-name">{{ $user->name }}</h4>
-							</a>
-						</div>
-                     </div>
-                 </div>
-                 @endforeach
+        <div class="container pt-60">
+               <div class="main-form-search">
+                    <form action="#" method="GET">
+                        <!-- <label>Search:</label> -->
+                        <input id="fsearch" type="text"  name="fsearch" value="" class="li-search-field" placeholder="Search a specific Vendor" style="float:right;">             
+                    </form>
+                </div>
             </div>
-           
-        </div>
+       <div class="container pt-60">
+                            
+            <div class="row userss">
+            @foreach ($users as $user)
+                    <div class="col-lg-3">
+                        <div class="featured">
+                            <div class="featured-vendor">
+                                <a href="{{ route('front.vendor',str_replace(' ', '-', $user->shop_name)) }}">
+                                    <div class="cat-img">
+                                    <img src="{{ $user->photo ? asset('assets/images/users/'.$user->photo):asset('assets/images/noimage.png') }}" />
+                                    
+                                </div>
+
+                                <hr>
+                                    <h4 class="vendor-name">{{ $user->name }}</h4>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+
         <div class="container">
             <div class="pagination">
         {!! $users->links() !!}
