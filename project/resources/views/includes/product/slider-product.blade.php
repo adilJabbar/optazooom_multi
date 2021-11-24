1a1ac05c72ba9ca030bbc8bfecb2c0de3feb1328
adilJabbar <46170020+adilJabbar@users.noreply.github.com>
2021-11-24 17:38:18 +0500

    coommaa

--- project/resources/views/includes/product/slider-product.blade.php
+++ project/resources/views/includes/product/slider-product.blade.php
@@ -36,6 +36,8 @@
 			</div>
 			@if($prod->thumbnail)
 			<img class="img-fluid" src="{{ $prod->thumbnail ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="">
+			@else if($prod->photo)
+			<img class="img-fluid" src="{{asset('assets/images/thumbnails/'.$prod->photo)}}" alt="">
 			@else
 			<img class="img-fluid" src="{{ $prod->photo ? asset('assets/images/products/'.$prod->photo):asset('assets/images/noimage.png') }}" alt="">
 
