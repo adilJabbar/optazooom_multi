b5177aa67685ed4256228e849dd31c95cd4fca37
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2021-11-24 15:46:35 +0500

    commit

--- project/resources/views/includes/product/slider-product.blade.php
+++ project/resources/views/includes/product/slider-product.blade.php
@@ -34,12 +34,10 @@
 					</li>
 				</ul>
 			</div>
-			
-			@if($prod->thumbnail)
 			<img class="img-fluid" src="{{ $prod->thumbnail ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="">
-			@else
+			<!-- @else
 			<img class="img-fluid" src="{{ $prod->photo ? asset('assets/images/products/'.$prod->photo):asset('assets/images/noimage.png') }}" alt="">
-			@endif
+			@endif -->
 		</div>
 		<div class="info">
 			<div class="stars">
