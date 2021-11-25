b2b3b602e35a6b2acd4a906c72536e3f4d6720b5
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2021-11-25 18:37:58 +0500

    commit

--- project/resources/views/includes/product/home-product.blade.php
+++ project/resources/views/includes/product/home-product.blade.php
@@ -38,7 +38,11 @@
 															</li> -->
 														</ul>
 													</div>
+													@if($prod->thumbnail)
 												<img class="img-fluid" src="{{ $prod->thumbnail ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="">
+												@else
+												<img class="img-fluid" src="{{ $prod->photo ? asset('assets/images/products/'.$prod->photo):asset('assets/images/noimage.png') }}" alt="">
+												@endif
 											</div>
 											<div class="info">
 												<div class="stars">
