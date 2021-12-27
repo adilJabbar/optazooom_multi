5bc9c08232b2b993a3a34a7fe32ea015bb88bdeb
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2021-12-27 14:59:20 +0500

    commit

--- project/resources/views/includes/product/slider-product.blade.php
+++ project/resources/views/includes/product/slider-product.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('front.product', $prod->slug) }}" class="item">
 		<div class="item-img">
-			@if(!empty($prod->features))
+			@if(!isset($prod->features))
 				<div class="sell-area">
 				@foreach($prod->features as $key => $data1)
 					<span class="sale" style="background-color:{{ $prod->colors[$key] }}">{{ $prod->features[$key] }}</span>
