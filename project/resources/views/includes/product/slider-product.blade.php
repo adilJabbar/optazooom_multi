eba697c5e4ad7d8d75b0ab47dd5645b7bd1721b1
Sajjad <56935657+Sajjad750@users.noreply.github.com>
2021-12-31 15:10:11 +0500

    cmt

--- project/resources/views/includes/product/slider-product.blade.php
+++ project/resources/views/includes/product/slider-product.blade.php
@@ -1,52 +1,155 @@
 <a href="{{ route('front.product', $prod->slug) }}" class="item">
-		<div class="item-img">
-			@if(!isset($prod->features))
-				<div class="sell-area">
-				@foreach($prod->features as $key => $data1)
-					<span class="sale" style="background-color:{{ $prod->colors[$key] }}">{{ $prod->features[$key] }}</span>
-					@endforeach 
-				</div>
-			@endif
-			<div class="extra-list">
-				<ul>
-					<li>
-						@if(Auth::guard('web')->check())
+<?php $email = DB::table('users')->where('id',$prod->user_id)->first('email');
+                                        $email = $email->email;
+										if($email == 'ozcaprioptics@gmail.com')
+										{
+										?>
+										
+											<div class="item-img">
+												@if(!empty($prod->features))
+													<div class="sell-area">
+													  @foreach($prod->features as $key => $data1)
+														<span class="sale" style="background-color:{{ $prod->colors[$key] }}">{{ $prod->features[$key] }}</span>
+														@endforeach
+													</div>
+												@endif
+													<div class="extra-list">
+														<ul>
+															<li>
+																@if(Auth::guard('web')->check())
 
-						<span class="add-to-wish" data-href="{{ route('user-wishlist-add',$prod->id) }}" data-toggle="tooltip" data-placement="right" title="{{ $langg->lang54 }}" data-placement="right"><i class="icofont-heart-alt" ></i>
-						</span>
+																<span class="add-to-wish" data-href="{{ route('user-wishlist-add',$prod->id) }}" data-toggle="tooltip" data-placement="right" title="{{ $langg->lang54 }}" data-placement="right"><i class="icofont-heart-alt" ></i>
+																</span>
 
-						@else 
+																@else
 
-						<span rel-toggle="tooltip" title="{{ $langg->lang54 }}" data-toggle="modal" id="wish-btn" data-target="#comment-log-reg" data-placement="right">
-							<i class="icofont-heart-alt"></i>
-						</span>
+																<span rel-toggle="tooltip" title="{{ $langg->lang54 }}" data-toggle="modal" id="wish-btn" data-target="#comment-log-reg" data-placement="right">
+																	<i class="icofont-heart-alt"></i>
+																</span>
 
-						@endif
-					</li>
-					<li>
-						@if(Auth::user())
-					<span class="quick-view" rel-toggle="tooltip" title="{{ $langg->lang55 }}" href="javascript:;" data-href="{{ route('product.quick',$prod->id) }}" data-toggle="modal" data-target="#quickview" data-placement="right"> <i class="icofont-eye"></i>
-					</span>
-					@endif
-					</li>
-				</ul>
-			</div>
-			<?php  
-			$img = explode(',',$prod->photo);
-            if(strpos($prod->thumbnail,'png') || strpos($prod->thumbnail,'jpg') || strpos($prod->thumbnail,'jpeg')) {
-			 ?>	
+																@endif
+															</li>
+															<li>
+															@if(Auth::user())
+															<span class="quick-view" rel-toggle="tooltip" title="{{ $langg->lang55 }}" href="javascript:;" data-href="{{ route('product.quick',$prod->id) }}" data-toggle="modal" data-target="#quickview" data-placement="right"> <i class="icofont-eye"></i>
+															</span>
+															@endif
+															</li>
+															<!-- <li>
+																<span class="add-to-compare" data-href="{{ route('product.compare.add',$prod->id) }}"  data-toggle="tooltip" data-placement="right" title="{{ $langg->lang57 }}" data-placement="right">
+																	<i class="icofont-exchange"></i>
+																</span>
+															</li> -->
+														</ul>
+													</div>
+													
+													
+																<?php  
 
-			@if($prod->thumbnail)	
-			<img class="img-fluid" src="{{ $prod->thumbnail ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="">
-			 @else
-				<img class="img-fluid" src="{{ $prod->photo ? asset('assets/images/products/'.$prod->photo):asset('assets/images/noimage.png') }}" alt="">
-			@endif
-				<?php }
-				
-				else {  ?>
-				
-				<img class="img-fluid" src="{{ $prod->thumbnail ? $prod->thumbnail:asset('assets/images/noimage.png') }}" alt="">	 <?php  }?>
-		</div>
+																$img = explode(',',$prod->photo);
+
+																if(isset($img[0]) && strpos($img[0],'png') || strpos($img[0],'jpg') || strpos($img[0],'jpeg')) { ?>
+																<!-- @if($img[0]) -->
+																
+															
+																<img class="img-fluid" src="{{ $img[0] ? asset('assets/images/products/'.$img[0]):asset('assets/images/noimage.png') }}" alt="" style="height:200px;">
+														<!-- 	     @else
+																<img class="img-fluid" src="{{ $prod->photo ? asset('assets/images/products/'.$prod->photo):asset('assets/images/noimage.png') }}" alt="">
+																@endif -->
+
+
+															<?php }else{ 
+
+
+																	$img = explode(',',$prod->photo);
+																if(isset($img[0]) && strpos($img[0],'images')) 
+																{ ?>
+															<img class="img-fluid" src="{{ $img[0] ? asset('assets/images/products/'.$img[0]):asset('assets/images/noimage.png') }}" alt="">
+														<?php		}else{
+															
+														
+																?>
+																			<img class="img-fluid" src="@if(isset($img[0])) {{ $img[0] }} @endif" alt="">
+																					
+
+														<?php 	}
+														
+														}?>
+											
+											</div>
+												  <?php } else { ?>
+													<div class="item-img">
+												@if(!empty($prod->features))
+													<div class="sell-area">
+													  @foreach($prod->features as $key => $data1)
+														<span class="sale" style="background-color:{{ $prod->colors[$key] }}">{{ $prod->features[$key] }}</span>
+														@endforeach
+													</div>
+												@endif
+													<div class="extra-list">
+														<ul>
+															<li>
+																@if(Auth::guard('web')->check())
+
+																<span class="add-to-wish" data-href="{{ route('user-wishlist-add',$prod->id) }}" data-toggle="tooltip" data-placement="right" title="{{ $langg->lang54 }}" data-placement="right"><i class="icofont-heart-alt" ></i>
+																</span>
+
+																@else
+
+																<span rel-toggle="tooltip" title="{{ $langg->lang54 }}" data-toggle="modal" id="wish-btn" data-target="#comment-log-reg" data-placement="right">
+																	<i class="icofont-heart-alt"></i>
+																</span>
+
+																@endif
+															</li>
+															<li>
+															@if(Auth::user())
+															<span class="quick-view" rel-toggle="tooltip" title="{{ $langg->lang55 }}" href="javascript:;" data-href="{{ route('product.quick',$prod->id) }}" data-toggle="modal" data-target="#quickview" data-placement="right"> <i class="icofont-eye"></i>
+															</span>
+															@endif
+															</li>
+															<!-- <li>
+																<span class="add-to-compare" data-href="{{ route('product.compare.add',$prod->id) }}"  data-toggle="tooltip" data-placement="right" title="{{ $langg->lang57 }}" data-placement="right">
+																	<i class="icofont-exchange"></i>
+																</span>
+															</li> -->
+														</ul>
+													</div>
+													
+													
+																<?php  
+
+																$img = explode(',',$prod->photo);
+
+																if(isset($img[0]) && strpos($img[0],'png') || strpos($img[0],'jpg') || strpos($img[0],'jpeg')) { ?>
+																<!-- @if($img[0]) -->
+																
+															
+																<img class="img-fluid" src="{{ $img[0] ? asset('assets/images/products/'.$img[0]):asset('assets/images/noimage.png') }}" alt="">
+														<!-- 	     @else
+																<img class="img-fluid" src="{{ $prod->photo ? asset('assets/images/products/'.$prod->photo):asset('assets/images/noimage.png') }}" alt="">
+																@endif -->
+
+
+															<?php }else{ 
+
+
+																	$img = explode(',',$prod->photo);
+																if(isset($img[0]) && strpos($img[0],'images')) 
+																{ ?>
+															<img class="img-fluid" src="{{ $img[0] ? asset('assets/images/products/'.$img[0]):asset('assets/images/noimage.png') }}" alt="">
+														<?php		}else{
+															
+														
+																?>
+																			<img class="img-fluid" src="@if(isset($img[0])) {{ $img[0] }} @endif" alt="">
+																					
+
+														<?php 	}
+														
+														}?>
+											</div>
+											<?php } ?>
 		<div class="info">
 			<div class="stars">
 				<div class="ratings">
